6a08ecf40f7d05354c2a914d6ccda9957f8aff22
Mandy Liu <mandyliu040@gmail.com>
2023-11-29 20:16:18 +0800

    encrypter python file

---
+++ encryption.py
@@ -0,0 +1,15 @@
+"""class encrypter()
+
+methods RSA 256 bit   
+string --> enc 
+enc --> string
+
+attributes 
+def __init__(self, key):
+    self.public_key : str 
+    self.private_key : str
+    
+look up: type annotation python
+
+public and private key 
+"""
