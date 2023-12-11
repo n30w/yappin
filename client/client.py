a9012974eb16214b7aee37cfb473829631f4c516
n30w <rocketneo@gmail.com>
2023-12-11 18:20:25 +0800

    Client command line updates

--- client/client.py
+++ client/client.py
@@ -3,6 +3,7 @@ Physical application that is the client.
 """
 
 import select
+import sys
 from shared.encryption import Key, Locksmith
 from shared.protobuf import Action, deserialize, serialize
 from shared.message import DataMessage
@@ -36,7 +37,6 @@ def sanitize_input(input: str) -> tuple[str, str]:
 class Client(Pluggable):
     def __init__(self, config: Config, username: str) -> None:
         super().__init__(config)
-        self.__config = config
         self.__username = username
 
         # Encryption
@@ -48,69 +48,118 @@ class Client(Pluggable):
     def _handle_incoming_message(self, message: DataMessage) -> None:
         # server sent messages will always have params coming back AND have response code AND have sender that is "SERVER"
         if message.sender == "SERVER":
-            print(message.params)
+            # Error has occurred
+            if message.response.response == 1:
+                print(f"[~] Uh oh! {message.response.comment}")
+
+    def _match_commands(self, user_input: str) -> (bytes, bool):
+        exited = False
+        msg: bytes = None
+        make_message = from_sender(self.__username, self.__serialized_pub_key)
+
+        if user_input[0] == "/":
+            command, params = sanitize_input(user_input)
+            if command not in COMMAND_BANK:
+                print("[~] Invalid command")
+            else:
+                match command:
+                    case "commands":
+                        print(f"Available commands:\n\t{COMMAND_BANK}")
+
+                    case "quit":
+                        print("[~] Quitting...")
+                        exited = True
+                        msg = make_message("", "", A_LOGOUT)
+                        message_ready = True
+
+                    case "chat":
+                        print(f"[~] Attempting connection with {params}...")
+                        msg = make_message(params, "", A_CONNECT)
+
+                    case _:
+                        print("[~] Invalid command")
+
+        return msg, exited
 
     def run(self):
         make_message = from_sender(self.__username, self.__serialized_pub_key)
 
-        exited = False
+        exit = False
 
-        print("\n~================~\n")
+        print("\n~================================================~\n")
         print(f" v(0 o 0)v yappin' as @{self.__username}\n")
         print(f"RSA Public Key: \n{self.__serialized_pub_key}")
-        print("~================~\n")
+        print("~================================================~\n")
 
         # Initial login handshake
         self.socket.connect()
         # msg = make_message("hello", "params", A_MESSAGE)
         msg = make_message("hello", "params", A_LOGIN)
-        self.socket.transmit(msg)
+        self.send_data(msg)
+        rec = deserialize(self.get_data())
+        sys_print(f"Connected successfully! MOTD: {rec.response.comment}")
 
         try:
-            while not exited:
-                action_queued: bool = False
+            while not exit:
+                message: bytes
                 command: str
                 params: str
+                message_ready: bool = False
 
-                read, _write, _error = select.select([self.get_socket()], [], [], 0)
+                read, _write, _error = select.select(
+                    [self.get_socket(), sys.stdin], [], [], 0
+                )
 
-                if self.get_socket() in read:
-                    data = self.get_data()
-                    if data is not None:
-                        incoming_message = deserialize(data)
-                        print(incoming_message)
-                        self._handle_incoming_message(incoming_message)
+                for source in read:
+                    """
+                    If the source is from the client network socket, handle it.
+                    """
+                    if source is self.get_socket():
+                        data = self.get_data()
+                        if data is not None:
+                            incoming_message = deserialize(data)
+                            self._handle_incoming_message(incoming_message)
 
-                user_input = input("[:>> ")
+                    """
+                    Checks for user input. This is on a different socket.
+                    """
+                    if source is sys.stdin:
+                        user_input = input()
 
-                if user_input[0] == "/":
-                    command, params = sanitize_input(user_input)
-                    if command not in COMMAND_BANK:
-                        print("invalid command!")
-                    else:
-                        match command:
-                            case "commands":
-                                print(f"Available commands:\n\t{COMMAND_BANK}")
+                        if user_input[0] == "/":
+                            command, params = sanitize_input(user_input)
+                            if command not in COMMAND_BANK:
+                                sys_print(
+                                    "Invalid command. To see commands, type /commands"
+                                )
+                            else:
+                                match command:
+                                    case "commands":
+                                        sys_print(
+                                            f"Available commands:\n\t{COMMAND_BANK}"
+                                        )
 
-                            case "quit":
-                                print("[~] Quitting...")
-                                action_queued = True
-                                exited = True
-                                msg = make_message("", "", A_LOGOUT)
+                                    case "quit":
+                                        sys_print("Closing yappin'")
+                                        exit = True
+                                        msg = make_message("", "", A_LOGOUT)
+                                        message_ready = True
 
-                            case "chat":
-                                msg = make_message(params, "", A_CONNECT)
-                                print(f"[~] Attempting connection with {params}...")
-                                action_queued = True
+                                    case "chat":
+                                        sys_print(
+                                            f"Attempting connection with {params}"
+                                        )
+                                        msg = make_message(params, "", A_CONNECT)
+                                        message_ready = True
 
-                            case _:
-                                print("[~] Invalid command")
+                                    case _:
+                                        sys_print("invalid command")
 
-                if action_queued:
+                if message_ready:
                     self.send_data(msg)
 
         except KeyboardInterrupt:
-            print("quitting...")
+            sys_print("Yappin' closed via KeyboardInterrupt")
 
             # send quit code to server!
             msg = make_message("", "", A_LOGOUT)
@@ -118,9 +167,14 @@ class Client(Pluggable):
 
         except Exception as e:
             print(f"Error: {e}")
+            traceback.print_exc()
 
         finally:
             self.socket.close()
+
+
+def sys_print(text: str) -> None:
+    print(f"[~] {text}")
 
 
 def from_sender(sender: str, pub_key: str):
