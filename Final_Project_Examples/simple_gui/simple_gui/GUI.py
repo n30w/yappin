028a83e2db1e1acef1ebd7f48f3bdd57bde4acef
Mandy Liu <117441088+yamdn@users.noreply.github.com>
2023-12-13 12:04:50 +0800

    lol

--- Final_Project_Examples/simple_gui/simple_gui/GUI.py
+++ Final_Project_Examples/simple_gui/simple_gui/GUI.py
@@ -7,12 +7,8 @@ Created on Fri Apr 30 13:36:58 2021
 """
 
 # import all the required  modules
-<<<<<<< HEAD
 import re 
 
-=======
-import re
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
 import threading
 import select
 from tkinter import *
@@ -41,29 +37,17 @@ class GUI:
         self.my_msg = ""
         self.system_msg = ""
 
-<<<<<<< HEAD
     """ +++++++++++++++++++ Login Window +++++++++++++++++++ """
-=======
-    """ ====================== Login Window ====================== """
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
     def login(self):
         self.login = Toplevel()
 
         # set the prompt message for the user to enter name and password
         self.login.title("Login & Password")
-<<<<<<< HEAD
         self.login.resizable(width = False, height = False)
         self.login.configure(width = 400, height = 300)
         
         """====================== Name ======================"""
-=======
-        self.login.resizable(width=False, height=False)
-        self.login.configure(width=400, height=300)
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         # create the prompt title and sets where the label will be placed in the window
-<<<<<<< HEAD
         self.title = Label(self.login, 
                        text = "Please login to continue",
                        justify = CENTER, 
@@ -71,19 +55,7 @@ class GUI:
         
         self.title.place(relheight = 0.15, relx = 0.2, rely = 0.07)
         
-=======
-        self.title = Label(
-            self.login,
-            text="Please login to continue",
-            justify=CENTER,
-            font="Helvetica 14 bold",
-        )
-
-        self.title.place(relheight=0.13, relx=0.2, rely=0.07)
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         # create a NAME label and sets where it will be placed in the window
-<<<<<<< HEAD
         self.labelName = Label(self.login,
                                text = "Name: ",
                                font = "Helvetica 12")
@@ -100,21 +72,7 @@ class GUI:
         
         """====================== Password ======================"""
         
-=======
-        self.labelName = Label(self.login, text="Name: ", font="Helvetica 12")
-
-        self.labelName.place(relheight=0.2, relx=0.1, rely=0.2)
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         # create a PASSWORD label and sets where it will be placed in the window
-<<<<<<< HEAD
-        self.labelPassword = Label(self.login,
-                              text = "Password: ",
-                              font = "Helvetica 12")
-        
-        self.labelPassword.place(relheight = 0.2, relx = 0.1, rely = 0.4)
-          
-=======
         self.labelPassword = Label(self.login, text="Password: ", font="Helvetica 12")
 
         self.labelPassword.place(relheight=0.2, relx=0.1, rely=0.35)
@@ -124,18 +82,12 @@ class GUI:
 
         self.entryName.place(relwidth=0.5, relheight=0.12, relx=0.35, rely=0.2)
 
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         # create an entry box for inputing the password
-<<<<<<< HEAD
         self.entryPassword = Entry(self.login, font = "Helvetica 14", show = "*")
     
         self.entryPassword.place(relwidth = 0.4, relheight = 0.12,
                              relx = 0.35, rely = 0.4)
-=======
-        self.entryPassword = Entry(self.login, font="Helvetica 14", show="*")
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
 
-<<<<<<< HEAD
         """====================== Signup ======================"""
 
         self.Signup = Button(self.login,
@@ -157,35 +109,10 @@ class GUI:
         """====================== Bindings ======================"""
         self.entryName.bind('<Return>', lambda event: self.goAhead(self.entryName.get(), self.entryPassword.get()))
         self.entryPassword.bind('<Return>', lambda event: self.goAhead(self.entryName.get(), self.entryPassword.get()))
-=======
-        self.entryPassword.place(relwidth=0.5, relheight=0.12, relx=0.35, rely=0.37)
-
-        # set the focus of the curser
-        # not really sure what this does lmao
-        self.entryName.focus()
-
-        # create a Continue Button to move onto the Chat window
-        # BUT FIRST CHECKS the username and password with the .goAhead() funcation !!!!!
-        self.go = Button(
-            self.login,
-            text="CONTINUE",
-            font="Helvetica 14 bold",
-            command=lambda: self.goAhead(
-                self.entryName.get(), self.entryPassword.get()
-            ),
-        )
-
-        self.go.place(relx=0.4, rely=0.55)
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         self.Window.mainloop()
-<<<<<<< HEAD
         
     """ +++++++++++++++++++ To Server Side +++++++++++++++++++ """
-=======
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
     def goAhead(self, name, password):
-<<<<<<< HEAD
         # if eveything is satisfied then send the login-user to the server
     
         """ ====== Check if the user is already registered or not ======"""
@@ -238,14 +165,7 @@ class GUI:
         password_special_char = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
         password_num = re.compile('[0-9]')
         password_uppercase = re.compile('[A-Z]')
-=======
-        # checks for strong password
-        password_special_char = re.compile("[@_!#$%^&*()<>?/\|}{~:]")
-        password_num = re.compile("[0-9]")
-        password_uppercase = re.compile("[A-Z]")
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
 
-<<<<<<< HEAD
         if len(name) > 0 and len(password) >= 10 and password_special_char.search(password) and password_num.search(password) and password_uppercase.search(password):
             # Read existing content from the file
             try:
@@ -271,74 +191,12 @@ class GUI:
         
     """ +++++++++++++++++++ Chat Window +++++++++++++++++++"""       
   
-=======
-        # if eveything is satisfied then send the login-user to the server
-        if (
-            len(name) > 0
-            and len(password) >= 10
-            and password_special_char.search(password)
-            and password_num.search(password)
-            and password_uppercase.search(password)
-        ):
-            msg = json.dumps({"action": "login", "name": name, "password": password})
-            self.send(msg)
-            response = json.loads(self.recv())
-
-            if response["status"] == "ok":
-                self.login.destroy()
-                self.sm.set_state(S_LOGGEDIN)
-                self.sm.set_myname(name)
-                self.layout(name)
-                self.textCons.config(state=NORMAL)
-                # self.textCons.insert(END, "hello" +"\n\n")
-                self.textCons.insert(END, menu + "\n\n")
-                self.textCons.config(state=DISABLED)
-                self.textCons.see(END)
-                # while True:
-                #     self.proc()
-
-                # the thread to receive messages
-                process = threading.Thread(target=self.proc)
-                process.daemon = True
-                process.start()
-
-            elif response["status"] == "wrong password":
-                messagebox.showerror("Error", "Incorrect password ")
-
-            elif response["status"] == "duplicate":
-                messagebox.showerror("Error", "Username already exists")
-
-        # if password is not strong enough then suggest a stronger password
-        elif (
-            len(password) < 10
-            or not password_special_char.search(password)
-            or not password_num.search(password)
-            or not password_uppercase.search(password)
-        ):
-            gen_password = "".join(
-                random.choices(
-                    string.ascii_letters + string.digits, k=random.randint(10, 30)
-                )
-            )
-            messagebox.showerror(
-                "Error",
-                "Password must be at least 10 characters long, contain at least one special character, contain at least one number, and one uppercase letter. \n\n Suggested Password: "
-                + gen_password,
-            )
-
-        # if username is not entered
-        # room for imporvement: suggest a new username
-        else:
-            messagebox.showerror("Error", "Please fill all the fields")
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
     # The main layout of the chat
     def layout(self, name):
         self.name = name
         # to show chat window
         self.Window.deiconify()
         self.Window.title("CHATROOM")
-<<<<<<< HEAD
         self.Window.resizable(width = False, height = False)
         self.Window.configure(width = 470, height = 550, bg = "#17202A")
         
@@ -361,62 +219,13 @@ class GUI:
           
         self.entryMsg = Entry(self.labelBottom, bg = "#2C3E50", fg = "#EAECEE", font = "Helvetica 13")
           
-=======
-        self.Window.resizable(width=False, height=False)
-        self.Window.configure(width=470, height=550, bg="#17202A")
-        self.labelHead = Label(
-            self.Window,
-            bg="#17202A",
-            fg="#EAECEE",
-            text=self.name,
-            font="Helvetica 13 bold",
-            pady=5,
-        )
-
-        self.labelHead.place(relwidth=1)
-        self.line = Label(self.Window, width=450, bg="#ABB2B9")
-
-        self.line.place(relwidth=1, rely=0.07, relheight=0.012)
-
-        self.textCons = Text(
-            self.Window,
-            width=20,
-            height=2,
-            bg="#17202A",
-            fg="#EAECEE",
-            font="Helvetica 14",
-            padx=5,
-            pady=5,
-        )
-
-        self.textCons.place(relheight=0.745, relwidth=1, rely=0.08)
-
-        self.labelBottom = Label(self.Window, bg="#ABB2B9", height=80)
-
-        self.labelBottom.place(relwidth=1, rely=0.825)
-
-        self.entryMsg = Entry(
-            self.labelBottom, bg="#2C3E50", fg="#EAECEE", font="Helvetica 13"
-        )
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         # place the given widget
         # into the gui window
-<<<<<<< HEAD
         self.entryMsg.place(relwidth = 0.74, relheight = 0.06, rely = 0.008, relx = 0.011)
-=======
-        self.entryMsg.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         self.entryMsg.focus()
-<<<<<<< HEAD
         self.entryMsg.bind('<Return>', lambda event: self.sendButton(self.entryMsg.get()))
           
-=======
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         # create a Send Button
-<<<<<<< HEAD
         self.buttonMsg = Button(self.labelBottom, text = "Send", font = "Helvetica 10 bold", 
                                 width = 20, bg = "#ABB2B9",
                                 command = lambda : self.sendButton(self.entryMsg.get()))
@@ -426,45 +235,17 @@ class GUI:
     
         self.textCons.config(cursor = "arrow")
           
-=======
-        self.buttonMsg = Button(
-            self.labelBottom,
-            text="Send",
-            font="Helvetica 10 bold",
-            width=20,
-            bg="#ABB2B9",
-            command=lambda: self.sendButton(self.entryMsg.get()),
-        )
-
-        self.buttonMsg.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
-
-        self.textCons.config(cursor="arrow")
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         # create a scroll bar
         scrollbar = Scrollbar(self.textCons)
 
         # place the scroll bar
         # into the gui window
-<<<<<<< HEAD
         scrollbar.place(relheight = 1, relx = 0.974)
         scrollbar.config(command = self.textCons.yview)
         self.textCons.config(state = DISABLED)
   
-=======
-        scrollbar.place(relheight=1, relx=0.974)
-
-        scrollbar.config(command=self.textCons.yview)
-
-        self.textCons.config(state=DISABLED)
-
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
     # function to basically start the thread for sending messages
     def sendButton(self, msg):
-<<<<<<< HEAD
-=======
-        self.textCons.config(state=DISABLED)
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
         self.my_msg = msg
         # print(msg)
         self.entryMsg.delete(0, END)
@@ -495,10 +276,6 @@ class GUI:
 
 
 # create a GUI class object
-<<<<<<< HEAD
 
 if __name__ == "__main__": 
-=======
-if __name__ == "__main__":
->>>>>>> 93e849d3d2b06e8d0f46b696e0893c5bf38b5087
     g = GUI()
